3d578b43fcd5c5e898d5d0b3a6e8494eeacc61d0
nadyac <nadyacpena@gmail.com>
2016-01-19 00:17:00 -0500

    sample code for connecting to mysql database using the python connector and creating some basic tables.

---
+++ login_SQL_cmds.py
@@ -0,0 +1,42 @@
+# loginPythonSQL.py
+
+#import the connector module and print function and errorcode
+from __future__ import print_function
+from mysql.connector import errorcode
+import mysql.connector
+
+DB_NAME = 'users_db'
+#create database
+def create_database(cursor):
+	try:
+		cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+	except mysql.connector.Error as err:
+		print("Failed creating database: {}".format(err))
+        exit(1)
+
+#connect to the database using username, password, host, database.
+try:
+	cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='users_db')
+	cursor = cnx.cursor()
+	print("Made connection.")
+
+#if there is an error connecting to the database, throw an error
+except mysql.connector.Error as err:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Could not connect to database with the given credentials.")
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("It seems that database does not exist. Will create it.")
+		cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1')
+		cursor = cnx.cursor()
+		create_database(cursor)
+	else:
+		# if not error with credentials or db existence, then print out err
+		print(err)
+
+#create tables dictionary
+
+#create tables if they don't exist.
+
+#close the connection.
+cursor.close()
+cnx.close()
---
+++ pythonSQL.py
@@ -0,0 +1,153 @@
+# pythonMySQL.py
+from __future__ import print_function
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+	cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='users_testing')
+	cursor = cnx.cursor()
+except mysql.connector.Error as err:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Something went wrong. Could not connect to database with the given credentials.")
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("Database does not exist.")
+	else:
+		print(err)
+else:
+		print("connection successful. Maybe.")
+
+''' 
+======================================================================
+Note when there are lots of args to the db connection, might
+	be best to store them in a dictionary and use the ** operator.
+======================================================================
+config = {
+  'user': 'scott',
+  'password': 'tiger',
+  'host': '127.0.0.1',
+  'database': 'employees',
+  'raise_on_warnings': True,
+}
+
+cnx = mysql.connector.connect(**config)
+=======================================================================
+'''
+try:
+	cnx = mysql.connector.connect(user='root',password='root', host='127.0.0.1')
+	# All DDL (Data Definition Language) statements are executed using a handle structure known as a cursor.
+	cursor = cnx.cursor()
+except mysql.connector.Error as err:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Something went wrong. Could not connect to database with the given credentials.")
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("Database does not exist.")
+	else:
+		print(err)
+else:
+	print("Second connection successful.")
+
+DB_NAME = 'users_testing'
+
+TABLES = {}
+TABLES['employees'] = (
+    "CREATE TABLE `employees` ("
+    "  `emp_no` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `birth_date` date NOT NULL,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `gender` enum('M','F') NOT NULL,"
+    "  `hire_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`)"
+    ") ENGINE=InnoDB")
+
+TABLES['departments'] = (
+    "CREATE TABLE `departments` ("
+    "  `dept_no` char(4) NOT NULL,"
+    "  `dept_name` varchar(40) NOT NULL,"
+    "  PRIMARY KEY (`dept_no`), UNIQUE KEY `dept_name` (`dept_name`)"
+    ") ENGINE=InnoDB")
+
+TABLES['salaries'] = (
+    "CREATE TABLE `salaries` ("
+    "  `emp_no` int(11) NOT NULL,"
+    "  `salary` int(11) NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`,`from_date`), KEY `emp_no` (`emp_no`),"
+    "  CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`emp_no`) "
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB")
+
+TABLES['dept_emp'] = (
+    "CREATE TABLE `dept_emp` ("
+    "  `emp_no` int(11) NOT NULL,"
+    "  `dept_no` char(4) NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`,`dept_no`), KEY `emp_no` (`emp_no`),"
+    "  KEY `dept_no` (`dept_no`),"
+    "  CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_no`) "
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,"
+    "  CONSTRAINT `dept_emp_ibfk_2` FOREIGN KEY (`dept_no`) "
+    "     REFERENCES `departments` (`dept_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB")
+
+TABLES['dept_manager'] = (
+    "  CREATE TABLE `dept_manager` ("
+    "  `dept_no` char(4) NOT NULL,"
+    "  `emp_no` int(11) NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`,`dept_no`),"
+    "  KEY `emp_no` (`emp_no`),"
+    "  KEY `dept_no` (`dept_no`),"
+    "  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) "
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,"
+    "  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) "
+    "     REFERENCES `departments` (`dept_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB")
+
+TABLES['titles'] = (
+    "CREATE TABLE `titles` ("
+    "  `emp_no` int(11) NOT NULL,"
+    "  `title` varchar(50) NOT NULL,"
+    "  `from_date` date NOT NULL,"
+    "  `to_date` date DEFAULT NULL,"
+    "  PRIMARY KEY (`emp_no`,`title`,`from_date`), KEY `emp_no` (`emp_no`),"
+    "  CONSTRAINT `titles_ibfk_1` FOREIGN KEY (`emp_no`)"
+    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE"
+    ") ENGINE=InnoDB")
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cnx.database = DB_NAME  
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+# iterate through the tables in the TABLES dictionary and add them to the database.  
+for name, ddl in TABLES.iteritems():
+    try:
+    	print("Creating table {}: ".format(name), end='')
+    	cursor.execute(ddl)
+    except mysql.connector.Error as err:
+    	if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+    		print("Table " + name + " already exists.")
+    	else:
+    		print(err.msg)
+    else:
+    	print("OK.")
+cursor.close()
+cnx.close()
