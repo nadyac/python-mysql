83a96d408e83bf401da146c4928aa020c52b4d3e
nadyac <nadyacpena@gmail.com>
2016-01-21 00:11:49 -0500

    working flask sample files.

---
+++ flaskfile.py
@@ -0,0 +1,39 @@
+from flask import Flask, url_for, redirect
+from flask import request #so we can process data entered by user
+app = Flask(__name__)
+# app.route tells Flask what url triggers the function or
+# what pages to serve for that location specified in route.
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username #uses python string format %s
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+# Uses built-in form attribute & request obj to handle form data
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+if __name__ == "__main__":
+	app.run()
